9df221c4679d6785e5841388b3dacc6066394b04
Haselnussbomber <mail@haselnussbomber.de>
2024-08-30 21:18:56 +0200

    update

--- DrawingTest/ClockNode.cs
+++ DrawingTest/ClockNode.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Xml;
+using HaselCommon.ImGuiYoga.Attributes;
 using HaselCommon.ImGuiYoga.Components;
-using HaselCommon.ImGuiYoga.Core;
 
 namespace DrawingTest;
 
-public class ClockNode : TextNode
+[YogaNode("clock")]
+public partial class ClockNode : TextNode
 {
+    private TextNode? TimeTextNode; // TODO: let generator create refs
+
     public string Format { get; set; } = "HH:mm";
 
     private DateTime LastDateTime = DateTime.MinValue;
 
-    public override void ApplyXmlAttribute(XmlAttribute attr, YogaContext context, XmlNode node)
+    public override void ApplyXmlAttribute(XmlAttribute attr, XmlNode node)
     {
         switch (attr.Name)
         {
@@ -20,13 +23,15 @@ public class ClockNode : TextNode
                 break;
 
             default:
-                base.ApplyXmlAttribute(attr, context, node);
+                base.ApplyXmlAttribute(attr, node);
                 break;
         }
     }
 
     public override void Update()
     {
+        TimeTextNode ??= GetNodeById<TextNode>("text");
+
         var now = DateTime.Now;
         if (LastDateTime != now)
         {
--- DrawingTest/TestWindow.cs
+++ DrawingTest/TestWindow.cs
@@ -1,28 +1,39 @@
-using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Client.UI;
-using HaselCommon;
 using HaselCommon.ImGuiYoga;
-using HaselCommon.ImGuiYoga.Components;
 using HaselCommon.ImGuiYoga.Core;
 using HaselCommon.Services;
+using Microsoft.Extensions.Logging;
 
 namespace DrawingTest;
 
 public class TestWindow : YogaWindow
 {
-    public TestWindow(WindowManager WindowManager) : base(WindowManager, "TestWindow")
+    public TestWindow(WindowManager WindowManager, ILogger<TestWindow> logger) : base(WindowManager, "TestWindow")
     {
+        Context.Logger = logger;
         Context.RegisterType<ClockNode>();
         RootNode = YogaLoader.LoadManifestResource(Context, "DrawingTest.TestWindow.xml");
 
-        var characterIconNode = RootNode?.GetNodeById<IconNode>("character-icon");
-        if (characterIconNode != null)
-        {
-            characterIconNode.OnClick = () => { unsafe { UIModule.Instance()->ExecuteMainCommand(2); } };
-        }
+        var characterIcon = RootNode?.GetNodeById("character-icon");
+        if (characterIcon != null)
+            characterIcon.Interactable = true;
         else
+            Context.Logger?.LogError("Node character-icon not found!");
+    }
+
+    public override bool OnEvent(YogaEventType eventType, YogaNode? sender, params object[] args)
+    {
+        switch (eventType)
         {
-            Service.Get<IPluginLog>().Warning("character-icon not found");
+            case YogaEventType.MouseClick when sender?.Id == "character-icon":
+                unsafe { UIModule.Instance()->ExecuteMainCommand(2); }
+                return true;
+
+            case YogaEventType.MouseClick:
+                Context.Logger?.LogTrace("Mouse click from {nodeDisplayName}!", sender?.DisplayName ?? "unknown node");
+                return true;
         }
+
+        return base.OnEvent(eventType, sender, args);
     }
 }
