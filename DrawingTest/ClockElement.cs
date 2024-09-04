696a151435fe7836b6ff3cf25c716fedbd3079d5
Haselnussbomber <mail@haselnussbomber.de>
2024-09-04 04:56:54 +0200

    5 am save

--- DrawingTest/ClockElement.cs
+++ DrawingTest/ClockElement.cs
@@ -1,44 +1,35 @@
 using System;
 using System.Text;
 using HaselCommon.ImGuiYoga;
+using HaselCommon.ImGuiYoga.Attributes;
 using Lumina.Text.ReadOnly;
 
 namespace DrawingTest;
 
-public partial class ClockElement : Text
+public class ClockElement : Text
 {
-    public override string NodeName => "clock";
-
-    private Text? TimeTextNode; // TODO: let generator create refs
-
-    public string Format { get; set; } = "HH:mm";
-
     private DateTime LastDateTime = DateTime.MinValue;
 
-    public override void ApplyXmlAttribute(string name, string value)
-    {
-        switch (name)
-        {
-            case "format":
-                Format = value;
-                break;
+    [NodeRef(".time")]
+    private Text? TimeTextNode;
 
-            default:
-                base.ApplyXmlAttribute(name, value);
-                break;
-        }
-    }
+    [NodeProperty]
+    public string Format { get; set; } = "HH:mm";
 
     public override void Update()
     {
-        TimeTextNode ??= GetNodeById<Text>("text");
+        if (IsDirty)
+        {
+            UpdateRefs();
+        }
+
+        TimeTextNode ??= QuerySelector<Text>("#text");
 
         var now = DateTime.Now;
         if (LastDateTime != now)
         {
             Data = new ReadOnlySeString(Encoding.UTF8.GetBytes(now.ToString(Format)));
             LastDateTime = now;
-            IsDirty = true;
         }
     }
 }
--- DrawingTest/TestWindow.cs
+++ DrawingTest/TestWindow.cs
@@ -19,12 +19,58 @@ public class TestWindow : Window
         Document = loader.FromManifestResource($"{type.Namespace}.{type.Name}.xml");
         Document.AddEventListener(OnEvent);
 
-        var sestringtestNode = Document.GetNodeById("sestringtest");
+        var sestringtestNode = Document.QuerySelector(".wrapper > #character-icon");
         if (sestringtestNode != null)
         {
+            logger.LogDebug("NODE FOUND! {displayName}", sestringtestNode.DisplayName);
             //sestringtestNode.FirstChild?.LastChild?.
             //    sestringtestTextNode.Text = "Test String";
         }
+        else
+        {
+            logger.LogDebug("node not found");
+        }
+
+        logger.LogDebug("TEST 1 START");
+        logger.LogDebug("TEST 1 RESULT: {displayName}", Document.QuerySelector(".selectortest > .first-child")?.DisplayName);
+        logger.LogDebug("TEST 2 START");
+        logger.LogDebug("TEST 2 RESULT: {displayName}", Document.QuerySelector(".selectortest > .child")?.DisplayName); // null
+        logger.LogDebug("TEST 3 START");
+        var firstChild = Document.QuerySelector(".selectortest .child");
+        logger.LogDebug("TEST 3 RESULT: {displayName}", firstChild?.DisplayName);
+        logger.LogDebug("TEST 3 NEXT: {displayName}", firstChild?.NextSibling?.DisplayName);
+        logger.LogDebug("TEST 4 START");
+        logger.LogDebug("TEST 4 RESULT: {displayName}", Document.QuerySelector(".selectortest .child + .child")?.DisplayName);
+
+        foreach (var node in Document.QuerySelectorAll(".selectortest .child ~ .child"))
+        {
+            logger.LogDebug("node for selector {selector}: {displayName}", ".selectortest .child ~ .child", node.DisplayName);
+        }
+
+        logger.LogDebug("TEST 5 START");
+        logger.LogDebug("TEST 5 RESULT: {displayName}", Document.QuerySelector(".child[id]")?.DisplayName);
+
+        logger.LogDebug("TEST 6 START");
+        logger.LogDebug("TEST 6 RESULT: {displayName}", Document.QuerySelector(".child[id=\"first-child-child2withid\"]")?.DisplayName);
+
+        logger.LogDebug("TEST 7 START");
+        logger.LogDebug("TEST 7 RESULT: {displayName}", Document.QuerySelector(".child[id~=\"first-child-child2withid\"]")?.DisplayName);
+
+        logger.LogDebug("TEST 8 START");
+        logger.LogDebug("TEST 8 RESULT: {displayName}", Document.QuerySelector(".child[id^=\"first-child\"]")?.DisplayName);
+
+        logger.LogDebug("TEST 9 START");
+        logger.LogDebug("TEST 9 RESULT: {displayName}", Document.QuerySelector(".child[id$=\"child2withid\"]")?.DisplayName);
+
+        logger.LogDebug("TEST 10 START");
+        logger.LogDebug("TEST 10 RESULT: {displayName}", Document.QuerySelector(".child[id*=\"child2\"]")?.DisplayName);
+
+        // TO BE IMPLEMENTED
+        logger.LogDebug("TEST 11 START");
+        logger.LogDebug("TEST 11 RESULT: {displayName}", Document.QuerySelector(".child:nth-child(2)")?.DisplayName);
+
+        logger.LogDebug("TEST 12 START");
+        logger.LogDebug("TEST 12 RESULT: {displayName}", Document.QuerySelector(".child:nth-child(odd)")?.DisplayName);
     }
 
     public unsafe void OnEvent(Event evt)
